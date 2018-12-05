87487a2548af06563c5998020de076a0eea6083a
Layalkashgari <Layalkash@gmail.com>
2018-12-05 18:45:16 +0300

    part1and2

--- nba_seed.sql
+++ nba_seed.sql
@@ -11,17 +11,20 @@ CREATE DATABASE nba_db;
 \c nba_db;
 
 -- Create table here
-
-
+CREATE TABLE players(id SERIAL , name text , age int , team text, games int , points int);
+ 
 -- Insert player here
 
+INSERT INTO players (id, name, age, team, games, points) values (default, 'Anderson Varejao', '29', 'CLE',  '25', '271');
 
-----------------------------------
--- Part 2
--- Uncomment COPY players statement and modify it with YOUR file path!
+--------------------------------
+Part 2
+Uncomment COPY players statement and modify it with YOUR file path!
 
 
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/trevorpreston/code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+COPY players
+  (name, age, team, games, points)
+
+FROM '
+'
+    DELIMITER ',' CSV;
