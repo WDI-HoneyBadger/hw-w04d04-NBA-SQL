fd13a4b06832e06f561d5b3b9e680907ff91606d
rawandahli <rawandahlii@gmail.com>
2018-12-05 23:30:53 +0300

    finished hw

--- nba_queries.sql
+++ nba_queries.sql
@@ -5,34 +5,33 @@
 
 -- All columns for all players from the New York Knicks (NYK).
 
-
+SELECT * FROM players WHERE team='NYK';
 
 -- All columns for all players from the Indiana Packers (IND) who are under 26
 -- years old.
-
+SELECT * FROM players where team='IND' AND age<26;
 
 
 -- All columns for all players, ordered from least points scored to most points
 -- scored.
 
-
+SELECT * FROM players order by points;
 
 -- Name and Points per game (points/games), for the players with the top 20 points
 -- per game.
 
-
+SELECT name, points FROM players order by points desc limit 20;
 
 -- The average age for all players.
 
-
+SELECT AVG(age) from players; 
 
 -- The average age for all players on the Oklahoma City Thunder (OKC).
-
+SELECT AVG(age) from players where team='OKC';
 
 
 -- The average age for all players who played more than 40 games.
-
-
+ 
 
 
 --------------------------------------------
--- nba_seed.sql
+++ nba_seed.sql
@@ -2,26 +2,27 @@
 -- Part 1
 
 -- Drop DB if exists
-DROP DATABASE IF EXISTS nba_db;
+-- DROP DATABASE IF EXISTS nba_db;
 
--- Create DB nba_db
-CREATE DATABASE nba_db;
+-- -- Create DB nba_db
+-- CREATE DATABASE nba_db;
 
 -- Connect to nba_db
 \c nba_db;
 
 -- Create table here
+-- CREATE TABLE  players(id SIRAL, name text, age INIT, team text, game INIT, points INIT);
 
-
--- Insert player here
-
+-- -- Insert player here(
+INSERT INTO players (id SERIAL, name text, age INT, team text, game INT, points INT) values (defult,'Anderson Varejao',29,'CLE',25,271);
 
 ----------------------------------
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
+-- (id , name , age , team , game , points)
 
+COPY players(name, age, team, games, points)
 
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/trevorpreston/code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+ 
+FROM '/Users/waleedettai/code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011-2012.csv'
+    DELIMITER ',' CSV;
