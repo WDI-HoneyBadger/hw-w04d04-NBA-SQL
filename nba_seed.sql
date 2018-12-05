c0ae083a28b12be6572fdc28a7c3747196327288
YA7MO <ya7yamo1@gmail.com>
2018-12-05 23:44:46 +0300

    done hw-w04d04

--- nba_seed.sql
+++ nba_seed.sql
@@ -2,26 +2,26 @@
 -- Part 1
 
 -- Drop DB if exists
-DROP DATABASE IF EXISTS nba_db;
+-- DROP DATABASE IF EXISTS nba_db;
 
 -- Create DB nba_db
-CREATE DATABASE nba_db;
+-- CREATE DATABASE nba_db;
 
 -- Connect to nba_db
 \c nba_db;
 
 -- Create table here
-
+-- CREATE TABLE players(name text , age int , team VARCHAR(3) , games int , points int);
 
 -- Insert player here
-
+-- INSERT INTO players(name,age,team,games,points) VALUES('Anderson Varejao',29,'CLE',25,271);
 
 ----------------------------------
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
 
 
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/trevorpreston/code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+COPY players
+  (name, age, team, games, points)
+FROM '/Users/yahyamo./code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011-2012.csv'
+    DELIMITER ',' CSV;
