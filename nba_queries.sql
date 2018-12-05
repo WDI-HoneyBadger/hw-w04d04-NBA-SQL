42b94694cbfad709d87fabeea8905d6947a8c1ea
A.K. Gothamey <Gothamey>
2018-12-05 20:57:44 +0300

    homework done

--- nba_queries.sql
+++ nba_queries.sql
@@ -5,34 +5,34 @@
 
 -- All columns for all players from the New York Knicks (NYK).
 
-
+select * from players where team = 'NYK';
 
 -- All columns for all players from the Indiana Packers (IND) who are under 26
 -- years old.
 
 
-
+select * from players where team = 'IND' and age < '26';
 -- All columns for all players, ordered from least points scored to most points
 -- scored.
 
-
+select * from players Order BY points ASC;
 
 -- Name and Points per game (points/games), for the players with the top 20 points
 -- per game.
-
+select name , points from players order by points desc limit 20;
 
 
 -- The average age for all players.
 
-
+select AVG(age) from players;
 
 -- The average age for all players on the Oklahoma City Thunder (OKC).
 
-
+select AVG(age) from players where team = 'OKC';
 
 -- The average age for all players who played more than 40 games.
 
-
+select AVG(age) from players where games > '40';
 
 
 --------------------------------------------
