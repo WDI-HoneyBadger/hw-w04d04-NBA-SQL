abc98dd5d4b4ffa0e2ea57953138bde7edba986c
marwah14 <memo_224@live.com>
2019-01-07 12:57:49 +0300

    done

--- nba_queries.sql
+++ nba_queries.sql
@@ -5,32 +5,38 @@
 
 -- All columns for all players from the New York Knicks (NYK).
 
+SELECT * FROM players where team='NYK';
 
 
 -- All columns for all players from the Indiana Packers (IND) who are under 26
 -- years old.
-
+SELECT * FROM players where team='IND' and age < 26;
 
 
 -- All columns for all players, ordered from least points scored to most points
 -- scored.
 
+SELECT * FROM players ORDER BY points ASC;
 
 
 -- Name and Points per game (points/games), for the players with the top 20 points
 -- per game.
+SELECT name, points FROM players ORDER BY points DESC LIMIT 20;
 
 
 
 -- The average age for all players.
+SELECT AVG(age) FROM players;
 
 
 
 -- The average age for all players on the Oklahoma City Thunder (OKC).
 
+SELECT AVG(age) FROM players WHERE team='OKC';
 
 
 -- The average age for all players who played more than 40 games.
+SELECT AVG(age) FROM players where games > 40;
 
 
 
--- nba_seed.sql
+++ nba_seed.sql
@@ -5,23 +5,33 @@
 DROP DATABASE IF EXISTS nba_db;
 
 -- Create DB nba_db
-CREATE DATABASE nba_db;
 
+CREATE DATABASE nba_db;
 -- Connect to nba_db
 \c nba_db;
 
 -- Create table here
-
+CREATE TABLE players(
+  name TEXT, 
+  age int, 
+  team TEXT,
+ games int,
+ points int
+);
 
 -- Insert player here
+INSERT INTO players VALUES ('Anderson Varejao', 29, 'CLE', 25, 271);
 
 
 ----------------------------------
 -- Part 2
 -- Uncomment COPY players statement and modify it with YOUR file path!
-
+--COPY players
+  --(name, age, team, games, points)
+--FROM '/macuser/trevorpreston/code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011_2012.csv'
+    --DELIMITER ',' CSV;
 
 --COPY players
   --(name, age, team, games, points)
---FROM '/Users/trevorpreston/code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011_2012.csv'
+--FROM '/Users/macuser/code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011_2012.csv'
     --DELIMITER ',' CSV;
